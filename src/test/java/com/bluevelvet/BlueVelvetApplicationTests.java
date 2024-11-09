9dcc2ec85c518abe8388c41ea72d67fba1dde882
0xbhsu <0xbhsu@hacari.net>
2024-11-09 12:20:46 -0300

    Initial Spring Boot application

---
+++ src/test/java/com/bluevelvet/BlueVelvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluevelvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueVelvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
