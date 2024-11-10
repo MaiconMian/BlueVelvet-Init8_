df3d4126e889cea618a027fcf93e8a7dbb4a91e4
0xbhsu <0xbhsu@hacari.net>
2024-11-10 13:36:04 -0300

    Updating docker configuration

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ rm -f *.jar
 mvn clean install -DskipTests
 cp target/*.jar bluevelvet.jar
 
-docker-compose build && MYSQL_DATABASE=${MYSQL_DATABASE:-mydatabase} MYSQL_PASSWORD=${MYSQL_PASSWORD:-rootpassword} docker-compose up
+docker-compose build && docker-compose up
