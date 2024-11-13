c0cb13b6604eaaddb6fd1b8c4bfca4aace468914
caiohmatos <caiohmatos369@gmail.com>
2024-11-12 22:01:41 -0300

    Add endpoint to create product

--- src/main/java/com/bluevelvet/controller/ProductController.java
+++ src/main/java/com/bluevelvet/controller/ProductController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 import com.bluevelvet.service.*;
 import com.bluevelvet.model.*;
+import com.bluevelvet.DTO.*;
 
 
 @RestController
@@ -18,6 +19,10 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductPhotosService productPhotosService;
+    @Autowired
+    private ProductDetailsService productDetailsService;
     @GetMapping("/products")
     public ResponseEntity<ApiResponse<List<Product>>>  getAllProducts(){
         List<Product> products = productService.getAllProducts();
@@ -30,7 +35,7 @@ public class ProductController {
     public ResponseEntity<ApiResponse<Product>> getProductById(@PathVariable int id) {
         Optional<Product> product = productService.getProductById(id);
         if (product.isPresent()) {
-            return ResponseEntity.ok(new ApiResponse<>("Success", "Product found", product.get());
+            return ResponseEntity.ok(new ApiResponse<>("Success", "Product found", product.get()));
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>("Error", "Product not found", null));
         }
@@ -44,6 +49,46 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ApiResponse<>("Error", "Product not found", null));
         }
+    }
+    @PostMapping("/addproduct")
+    public ResponseEntity<ApiResponse<Product>> addProduct(@RequestBody ProductDTO productDTO) {
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setImage(productDTO.getImage());
+        product.setShortDescription(productDTO.getShortDescription());
+        product.setLongDescription(productDTO.getLongDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setDiscount(productDTO.getDiscount());
+        product.setStatus(productDTO.getStatus());
+        product.setHasStock(productDTO.getHasStock());
+        product.setWidht(productDTO.getWidht());
+        product.setHeight(productDTO.getHeight());
+        product.setCost(productDTO.getCost());
+        product.setCreationTime(productDTO.getCreationTime());
+        product.setUpdateTime(productDTO.getUpdateTime());
+        product.setCategories(productDTO.getCategories());
+
+        productDTO.getDetails().forEach(productDetailsDTO -> {
+            ProductDetails productDetails = new ProductDetails();
+            productDetails.setDetailName(productDetailsDTO.getDetailName());
+            productDetails.setDetailValue(productDetailsDTO.getDetailValue());
+            productDetails.setProduct(product);
+            product.getDetails().add(productDetails);
+            productDetailsService.saveProductDetails(productDetails);
+        });
+
+        productDTO.getPhotos().forEach(productPhotosDTO -> {
+            ProductPhotos productPhotos = new ProductPhotos();
+            productPhotos.setImage(productPhotosDTO.getImage());
+            productPhotos.setProduct(product);
+            product.getPhotos().add(productPhotos);
+            productPhotosService.saveProductPhoto(productPhotos);
+        });
+
+
+        productService.saveProduct(product);
+
+        return ResponseEntity.ok(new ApiResponse<>("Success", "Product added successfully", product));
     }
 
 }
