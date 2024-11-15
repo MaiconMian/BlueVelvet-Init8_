f8bfcdefff15531967d208c0bd99b607a25c7032
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2024-11-15 12:32:10 -0300

    Adding repository classes for user and role

--- src/main/java/com/bluevelvet/repository/RoleRepository.java
+++ src/main/java/com/bluevelvet/repository/RoleRepository.java
@@ -1,2 +1,9 @@
-package com.bluevelvet.repository;public class RoleRepository {
+package com.bluevelvet.repository;
+
+import com.bluevelvet.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
--- src/main/java/com/bluevelvet/repository/UserRepository.java
+++ src/main/java/com/bluevelvet/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package com.bluevelvet.repository;public class UserRepository {
+package com.bluevelvet.repository;
+
+import com.bluevelvet.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    UserDetails findByEmail(String email);
 }
