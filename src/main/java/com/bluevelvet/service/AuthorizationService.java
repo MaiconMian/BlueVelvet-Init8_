f0036007f033deba5ecefa6b265131fc5aa938c1
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2024-11-15 12:33:26 -0300

    Adding service classes for users, roles and authentication

--- src/main/java/com/bluevelvet/service/AuthorizationService.java
+++ src/main/java/com/bluevelvet/service/AuthorizationService.java
@@ -1,2 +1,22 @@
-package com.bluevelvet.service;public class AutorizathionService {
+package com.bluevelvet.service;
+
+import com.bluevelvet.model.User;
+import com.bluevelvet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.bluevelvet.repository.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
+
 }
