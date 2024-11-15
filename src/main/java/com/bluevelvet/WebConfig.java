ac8d2b0d5e055555756f8c2036f757f9e64e99f5
leticiafreit <leticiafreitasdeoliveira23@hotmail.com>
2024-11-15 15:05:30 -0300

    Qualquer coisa

--- src/main/java/com/bluevelvet/WebConfig.java
+++ src/main/java/com/bluevelvet/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     }
