5f90006520df82837bc181b5aff7ef1a86de519e
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2024-11-15 21:54:40 -0300

    Adding methods to map dtos

--- src/main/java/com/bluevelvet/service/CategoryService.java
+++ src/main/java/com/bluevelvet/service/CategoryService.java
@@ -1,17 +1,25 @@
 package com.bluevelvet.service;
 
+import com.bluevelvet.model.Brand;
 import com.bluevelvet.model.Category;
+import com.bluevelvet.model.Product;
+import com.bluevelvet.repository.BrandRepository;
 import com.bluevelvet.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+import com.bluevelvet.DTO.CategoryDTO;
 
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private BrandService brandService;
+    @Autowired
+    private ProductService productService;
 
     public Category saveCategory(Category category) {
         return categoryRepository.save(category);
@@ -25,10 +33,12 @@ public class CategoryService {
         return null;
     }
 
-    public void deleteCategory(int id) {
+    public boolean deleteCategory(int id) {
         if (categoryRepository.existsById(id)) {
             categoryRepository.deleteById(id);
+            return true;
         }
+        return false;
     }
 
     public List<Category> getAllCategories() {
@@ -37,5 +47,47 @@ public class CategoryService {
 
     public Optional<Category> getCategoryById(int id) {
         return categoryRepository.findById(id);
+    }
+
+    public Category saveCategory (CategoryDTO categoryDTO){
+
+        Category newcategory = new Category();
+
+        newcategory.setImage(categoryDTO.getImage());
+        newcategory.setCategoryName(categoryDTO.getCategoryName());
+
+        this.saveCategory(newcategory);
+
+        if(categoryDTO.getCategoryParent() == null){
+            newcategory.setCategoryParent(newcategory);
+        } else {
+            Category parentCategory = this.getCategoryById(categoryDTO.getCategoryParent())
+                    .orElseThrow(() -> new IllegalArgumentException("Parent Category not fold"));
+            newcategory.setCategoryParent(parentCategory);
+        }
+
+        if (categoryDTO.getBrands() != null) {
+            categoryDTO.getBrands().forEach(brandId -> {
+                Brand brand = brandService.getBrandById(brandId)
+                        .orElseThrow(() -> new IllegalArgumentException("Brand not fold " + brandId));
+                newcategory.getBrands().add(brand);
+                brand.getCategory().add(newcategory);
+                brandService.saveBrand(brand);
+            });
+        }
+
+        if (categoryDTO.getProducts() != null) {
+            categoryDTO.getProducts().forEach(productId -> {
+                Product product = productService.getProductById(productId)
+                        .orElseThrow(() -> new IllegalArgumentException("Product not found for ID: " + productId));
+                newcategory.getProducts().add(product);
+                product.getCategories().add(newcategory);
+                productService.saveProduct(product);
+            });
+        }
+
+        this.saveCategory(newcategory);
+        return newcategory;
+
     }
 }
